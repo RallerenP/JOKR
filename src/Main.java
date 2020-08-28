a4a7e01ea404ab1062c0e586c395f95056644c94
rasmus povlsen <rasmus.alexander.povlsen@hotmail.dk>
2020-08-28 10:20:57 +0200

    Version 1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,29 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main
 {
-
     public static void main(String[] args)
     {
+        Main m = new Main();
 
+        m.displayRandom(m.getJokes());
+    }
+
+    private ArrayList<String> getJokes()
+    {
+        ArrayList<String> jokes = new ArrayList<>();
+        jokes.add("How does a scientist freshen their breath? With experi-mints");
+        jokes.add("What did the 0 say to the 8? Nice belt.");
+        jokes.add("Knock knock.\nWho's there? \nHatch. \nHatch who? \nBless you!");
+        jokes.add("Why did the melons plan a big wedding? Because they cantaloupe!");
+
+        return jokes;
+    }
+
+    private void displayRandom(ArrayList<String> strings)
+    {
+        int index = new Random().nextInt(strings.size());
+        System.out.println(strings.get(index));
     }
 }
